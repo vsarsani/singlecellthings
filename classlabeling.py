64c68339559d7187d18a6b15b35470abf79229a0
Vishal  Sarsani <vishal.statgen@gmail.com>
2024-06-11 06:58:04 -0400

    Update classlabeling.py

--- classlabeling.py
+++ classlabeling.py
@@ -59,14 +59,13 @@ def preprocess(sample_file, reference_file, sampleprefix):
     sample_filtered.obs['doublet_scores'] = doublet_scores
     sample_filtered.obs['predicted_doublets'] = predicted_doublets
 
-    # Check if 'predicted_doublets' exists and is not None
-    if 'predicted_doublets' in sample_filtered.obs and sample_filtered.obs['predicted_doublets'] is not None:
+    # Handle None values in 'predicted_doublets'
+    if 'predicted_doublets' in sample_filtered.obs:
         if sample_filtered.obs['predicted_doublets'].isnull().all():
-            print("Column 'predicted_doublets' is all None, proceeding without filtering.")
+            logging.warning("Column 'predicted_doublets' is all None, proceeding without filtering.")
         else:
             sample_filtered = sample_filtered[~sample_filtered.obs['predicted_doublets']].copy()
-    else:
-        print("Column 'predicted_doublets' not found or is None, proceeding without filtering.")
+
     # Filter out predicted doublets
     sample_filtered = sample_filtered[~sample_filtered.obs['predicted_doublets']].copy()
 
