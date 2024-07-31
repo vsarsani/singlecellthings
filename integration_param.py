f8a4f8ad31d284bacf11c3b8b7cc23ced3c08555
Vishal  Sarsani <vishal.statgen@gmail.com>
2024-07-31 17:43:52 -0400

    Update integration_param.py

--- integration_param.py
+++ integration_param.py
@@ -26,7 +26,7 @@ def main(args):
     adata = adata[:, keep]
     
     # Normalize, log transform, and find highly variable genes
-    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.normalize_total(adata, target_sum=None)
     sc.pp.log1p(adata)
     sc.pp.highly_variable_genes(adata, n_top_genes=args.n_top_genes, batch_key=args.batch_key)
     def scale_by_batch(adata: ad.AnnData, batch_key: str) -> ad.AnnData:
